49dd132566f2fc58a3f764ce1f946e36f1a93942
John DeSilvio <john.desilvio@yahoo.com>
2018-08-26 14:35:40 -0400

    Initial commit

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class="container">
+    <text class="title">Pin Bin</text>
+  </view>
+</template>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.title {
+  color: blue;
+  font-size: 32px;
+}
+.desc {
+  color: gray;
+  font-size: 16px;
+}
+</style>
