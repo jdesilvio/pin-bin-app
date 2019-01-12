32d568175aaf6f73a4a8f27883d1d3409044231c
John DeSilvio <john.desilvio@yahoo.com>
2019-01-12 13:04:15 -0500

    Update PinCard style

--- src/components/PinCard.vue
+++ src/components/PinCard.vue
@@ -1,15 +1,41 @@
 <template>
-  <view>
-    <view class="card">
-      <image
-        :style="{width: 50, height: 50}"
-        :source="{uri: imgUrl}"
-      />
-      <text :style="{fontSize: 16, textAlign: 'center', color: 'black'}">
+  <view
+    class="card"
+    :style="{
+      height: '80%',
+      width: '100%',
+      alignSelf: 'center',
+      borderWidth: 0.5,
+      borderRadius: 5,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.8,
+      shadowRadius: 2,
+      elevation: 1,
+    }"
+  >
+    <image
+      :style="{flex: 1, flexDirection: 'column', alignItems: 'flex-start'}"
+      :source="{uri: imgUrl}"
+    />
+    <view class="card-body" :style="{backgroundColor: 'white'}">
+      <text class="text" :style="{fontSize: 24, color: 'black'}">
         {{ name }}
       </text>
-      <text :style="{fontSize: 16, textAlign: 'center', color: 'black'}">
+      <text class="text" :style="{fontSize: 16, color: 'grey'}">
         {{ address }}
+      </text>
+    </view>
+
+    <view
+      class="card-footer"
+      :style="{
+        backgroundColor: '#D3D3D3',
+        borderBottomLeftRadius: 4,
+        borderBottomRightRadius: 4
+      }">
+      <text class="text" :style="{fontSize: 12, color: 'black'}">
+        {{ footerText }}
       </text>
     </view>
   </view>
@@ -20,19 +46,15 @@ export default {
   props: {
     imgUrl: String,
     name: String,
-    address: String
+    address: String,
+    footerText: String
   }
 }
 </script>
 
 <style>
-.card {
-  max-width: 960px;
-  padding: 16;
-  width: 95%;
-  border-radius: 10;
-  border-width: 0.5;
-  border-color: #d6d7da;
+.text {
+  text-align: center;
+  padding: 4;
 }
 </style>
-
--- src/screens/Pins.vue
+++ src/screens/Pins.vue
@@ -1,11 +1,12 @@
 <template>
   <view class="container">
     <nav-bar :navigation="navigation"></nav-bar>
-    <view>
+    <view :style="{padding: 16}">
       <pin-card
         img-url="https://upload.wikimedia.org/wikipedia/commons/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG"
         name="hi"
-        address="here">
+        address="here"
+        footer-text="Some footer text">
       </pin-card>
     </view>
   </view>
