d6e30c323ba3c7c8040e6943a9f3f0cf240389ce
John DeSilvio <john.desilvio@yahoo.com>
2019-01-11 23:28:26 -0500

    Add Pin cards

---
+++ src/components/PinCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <view>
+    <view class="card">
+      <image
+        :style="{width: 50, height: 50}"
+        :source="{uri: imgUrl}"
+      />
+      <text :style="{fontSize: 16, textAlign: 'center', color: 'black'}">
+        {{ name }}
+      </text>
+      <text :style="{fontSize: 16, textAlign: 'center', color: 'black'}">
+        {{ address }}
+      </text>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    imgUrl: String,
+    name: String,
+    address: String
+  }
+}
+</script>
+
+<style>
+.card {
+  max-width: 960px;
+  padding: 16;
+  width: 95%;
+  border-radius: 10;
+  border-width: 0.5;
+  border-color: #d6d7da;
+}
+</style>
+
---
+++ src/screens/Pins.vue
@@ -0,0 +1,30 @@
+<template>
+  <view class="container">
+    <nav-bar :navigation="navigation"></nav-bar>
+    <view>
+      <pin-card
+        img-url="https://upload.wikimedia.org/wikipedia/commons/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG"
+        name="hi"
+        address="here">
+      </pin-card>
+    </view>
+  </view>
+</template>
+
+<script>
+import PinCard from '../components/PinCard'
+import NavigationBar from '../components/NavigationBar'
+
+export default {
+  props: {
+    navigation: {
+      type: Object
+    }
+  },
+
+  components: {
+    PinCard,
+    NavBar: NavigationBar,
+  }
+}
+</script>
